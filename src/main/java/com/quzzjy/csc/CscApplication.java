f6f0fb7cb2a9a4c68da2945c231a6568248fb2d3
duliao <duliao116@163.com>
2019-10-23 20:17:14 +0800

    项目初始化

---
+++ src/main/java/com/quzzjy/csc/CscApplication.java
@@ -0,0 +1,13 @@
+package com.quzzjy.csc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CscApplication.class, args);
+	}
+
+}
